222f354182bb58e476ad027a7d22f288de8b1d39
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-05-30 13:51:40 +0200

    Add missing class postfix::params

---
+++ manifests/params.pp
@@ -0,0 +1,9 @@
+class postfix::params {
+    $ensure             = present
+    $ensure_running     = true
+    $ensure_enabled     = true
+    $manage_instances   = false
+    $config_source      = undef
+    $config_template    = undef
+    $instances          = undef
+}
