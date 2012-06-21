356f6067a74611c90bf5b469bdbdacde0730c221
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-06-21 15:13:22 +0200

    Bugfix: Actually use enable/ensure vars

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,12 +1,13 @@
 class postfix::service (
-    $ensure	= 'running',
-    $enabled	= true
+    $ensure     = 'running',
+    $enabled    = true
     ) inherits postfix::params {
 
     Class['postfix::package'] -> Class['postfix::service']
 
     service { 'postfix':
-        ensure      => running,
+        ensure      => $ensure,
+        enabled     => $enabled,
         hasrestart  => true,
         hasstatus   => true,
         require     => Package['postfix'],
